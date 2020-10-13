0550b1cc600ee89b6494df8e29ad42aafcdbe205
Bernhard Scholz <bernhard.scholz87@gmail.com>
2020-10-13 16:18:44 +0200

    Initial commit

---
+++ src/components/BlogText.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function BlogText() {
+    return (
+
+        /*<article className="BlogText">
+            <h2>10 reasons I'am happy as a goat!</h2>
+            <p className="sub-title">Subtitel</p>
+            <p className="text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+                sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+                Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+                sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+                Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                <br/>
+                    <button type="button">10 more reasons</button>
+            </p>
+            <img src="http://placegoat.com/300/500" alt="article-picture"/>
+        </article>
+
+         */
+    );
+}
+
+export default BlogText;
